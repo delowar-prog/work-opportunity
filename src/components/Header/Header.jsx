bcc0ce32aa3e25d8e6d0909ff9db262566791e35
Delowar Hossain <bscdelowar@yahoo.com>
2023-04-12 03:16:08 +0600

    Active link done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link, NavLink } from 'react-router-dom'
 
 
 const Header = () => {
@@ -9,10 +9,14 @@ const Header = () => {
                 <h1 className='text-lg md:text-2xl text-amber-500 font-bold'>WorkOpportunity</h1>
                 <nav>
                     <ul className='md:flex gap-7 text-gray-300 font-bold'>
-                        <li><Link to='/'>Home</Link></li>
-                        <li><Link to='/statistics'>Statistics</Link></li>
-                        <li><Link to='/applied-jobs'>Applied Jobs</Link></li>
-                        <li><Link to='/blogs'>Blogs</Link></li>
+                        <li><NavLink to='/' className={({ isActive}) =>isActive? "text-violet-500": ""
+                      }>Home</NavLink></li>
+                        <li><NavLink to='/statistics' className={({ isActive}) =>isActive? "text-violet-500": ""
+                    }>Statistics</NavLink></li>
+                        <li><NavLink to='/applied-jobs' className={({ isActive}) =>isActive? "text-violet-500": ""
+                    }>Applied Jobs</NavLink></li>
+                        <li><NavLink to='/blogs' className={({ isActive}) =>isActive? "text-violet-500": ""
+                    }>Blogs</NavLink></li>
                     </ul>
                 </nav>
                 <Link to=''><button className='main-btn-style'>Start Applying</button></Link>
